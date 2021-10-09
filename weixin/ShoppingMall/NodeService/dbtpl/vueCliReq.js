c6878983c64870bfd8b7bb982ad73e5d9d50781d
YiYaYiYaHei <1823972245@qq.com>
2021-10-09 17:47:30 +0800

    feature--vue-cli4-empty：1、添加token刷新机制（token加密、解密）;2、添加用户管理、系统日志接口

--- weixin/ShoppingMall/NodeService/dbtpl/vueCliReq.js
+++ weixin/ShoppingMall/NodeService/dbtpl/vueCliReq.js
@@ -28,13 +28,34 @@ const commonDeal = async (req, res, cb) => {
   }
 };
 
+// createCipheriv - key(32位随机)
+const tokenKey = 'abcdefg123456higklmn123456789opk';
+// createCipheriv - iv(16位随机)
+const tokenIv = 'rstuvwxyz1234567';
+// 生成token -- 对称加密
+function createToken(userName) {
+  // createCipheriv  key的长度取决于加密类型，这里用的aes-256-cbc所以得是32位(aes-128-cbc对应16位), iv始终16位
+  const cipher = crypto.createCipheriv('aes-256-cbc', tokenKey, tokenIv);
+  let crypted = cipher.update(JSON.stringify({userName, time: Date.now()}), "utf8", "base64");
+  crypted += cipher.final("base64");
+  return crypted;
+}
+// 解token
+function decryptToken(req) {
+  const token = req.headers.authorization.slice(7);
+  const cipher = crypto.createDecipheriv('aes-256-cbc', tokenKey, tokenIv);
+  let decrypted = cipher.update(token, "base64", "utf8");
+  decrypted += cipher.final("utf8");
+  return decrypted;
+}
+
 module.exports = app => {
   // 用户登录
   app.post('/login', (req, res) => commonDeal(req, res, () => {
-    const date = Date.now();
-    const role = date % 2 === 0 ? '管理员' : '普通用户';
+    const role = req.body.userName === 'admin' ? '管理员' : '普通用户';
     const data = {
-      token: crypto.createHash('sha256').update(req.body.userName).digest('hex'),
+      // 使用crypto.createHash('sha256').update(req.body.userName).digest('hex')，无法解密，因为hash是单向的
+      token: createToken(req.body.userName),
       role,
       userId: parseInt(Math.random() * 100)
     }
@@ -125,4 +146,45 @@ module.exports = app => {
       {value: 63, name: "黑龙江"}
     ]});
   }));
+  // 刷新token
+  app.get('/auth/refresh', (req, res) => commonDeal(req, res, () => {
+    const userName = (decryptToken(req) || {userName: 'null'}).userName;
+    return Object.assign({}, baseResponse, {data: createToken(userName)});
+  }));
+
+  // 系统管理-用户管理 - 表格数据
+  app.post('/user/table/list', (req, res) => commonDeal(req, res, () => {
+    const length = parseInt(Math.random() * 200);
+    const rows = [];
+    for(let i = 0; i < length; i++) {
+      rows.push({
+        createdTime: +new Date(),
+        userName: (i % 5 === 1 ? '张三' : i % 5 === 2 ? '李四' : i % 5 === 3 ? '隔壁王五' : i % 5 === 4 ? '老李头' : '小王头') + i,
+        role: i % 2 === 0 ? '管理员' : '普通用户',
+        describe: '用户的描述' + i,
+        id: i
+      });
+    };
+    return Object.assign({}, baseResponse, {data: {rows, total: length}});
+  }));
+  // 系统管理-用户管理 - 删除用户   接口/user/delete/30   获取id: req.params.id
+  app.delete('/user/delete/:id', (req, res) => commonDeal(req, res));
+  // 系统管理-用户管理 - 新增/删除用户
+  app.post('/user/addOrEdit', (req, res) => commonDeal(req, res));
+  // 系统管理-系统日志 - 表格数据
+  app.post('/system/log', (req, res) => commonDeal(req, res, () => {
+    const length = parseInt(Math.random() * 200);
+    const rows = [];
+    for(let i = 0; i < length; i++) {
+      rows.push({
+        createdTime: +new Date(),
+        name: '日志名称' + i,
+        userName: (i % 5 === 1 ? '张三' : i % 5 === 2 ? '李四' : i % 5 === 3 ? '隔壁王五' : i % 5 === 4 ? '老李头' : '小王头') + i,
+        path: '/user/operator/log',
+        id: i
+      });
+    };
+    return Object.assign({}, baseResponse, {data: {rows, total: length}});
+  }));
+
 };
